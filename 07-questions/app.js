8b696050ae8cd1e1b11de1753bf05091c6038db7
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-12-23 08:19:01 +0700

    Add functionality to toggle question text visibility using selectors

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,2 +1,22 @@
-//using selectors inside the element
-// traversing the dom
+// method 1: using selectors inside the element
+const questions = document.querySelectorAll(".question");
+questions.forEach((question) => {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
+
+// method 2: traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     e.currentTarget.parentElement.parentElement.classList.toggle("show-text");
+//   });
+// });
