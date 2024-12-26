14701596d16ea3e50167b37da0c66c932e2d7c97
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-12-26 09:16:19 +0700

    Add step value customization for increment and decrement

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,17 +1,20 @@
 let count = 0;
 let intervalId = null;
+let step = 1;
 
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
+const stepDropdown = document.querySelector("#step");
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const styles = e.currentTarget.classList;
+    step = parseInt(stepDropdown.value);
 
     if (styles.contains("decrease")) {
-      count--;
+      count -= step;
     } else if (styles.contains("increase")) {
-      count++;
+      count += step;
     } else if (styles.contains("auto-decrease")) {
       autoCounter("decrease");
     } else if (styles.contains("auto-increase")) {
@@ -45,9 +48,9 @@ const autoCounter = (type) => {
   stopCounter();
   intervalId = setInterval(() => {
     if (type === "increase") {
-      count++;
+      count += step;
     } else {
-      count--;
+      count -= step;
     }
     value.textContent = count;
     setColor();
