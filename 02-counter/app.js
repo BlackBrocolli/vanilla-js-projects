df2cbf63dcac8fe99e0ec07efbe65882a035b3d1
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-12-26 09:01:26 +0700

    Add auto increment/decrement and stop functionality

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,4 +1,5 @@
 let count = 0;
+let intervalId = null;
 
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
@@ -11,22 +12,51 @@ btns.forEach((btn) => {
       count--;
     } else if (styles.contains("increase")) {
       count++;
+    } else if (styles.contains("auto-decrease")) {
+      autoCounter("decrease");
+    } else if (styles.contains("auto-increase")) {
+      autoCounter("increase");
+    } else if (styles.contains("stop")) {
+      clearInterval(intervalId);
     } else {
       count = 0;
     }
 
-    if (count > 0) {
-      value.style.color = "green";
-    }
-
-    if (count < 0) {
-      value.style.color = "red";
-    }
-
-    if (count === 0) {
-      value.style.color = "#222";
-    }
-
     value.textContent = count;
+    setColor();
   });
 });
+
+const setColor = () => {
+  if (count > 0) {
+    value.style.color = "green";
+  }
+
+  if (count < 0) {
+    value.style.color = "red";
+  }
+
+  if (count === 0) {
+    value.style.color = "#222";
+  }
+};
+
+const autoCounter = (type) => {
+  stopCounter();
+  intervalId = setInterval(() => {
+    if (type === "increase") {
+      count++;
+    } else {
+      count--;
+    }
+    value.textContent = count;
+    setColor();
+  }, 1000);
+};
+
+const stopCounter = () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+};
